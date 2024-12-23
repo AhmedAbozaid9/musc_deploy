36a163409a37ce66c48e59a7ab6bd007c5313705
Ahmed <ahmedabozaid.dev@gmai.com>
2024-12-23 16:14:09 +0200

    adds user store

---
+++ store/useAuthStore.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useEffect } from "react";
+import { create } from "zustand";
+
+interface User {
+  name: string;
+  email: string;
+}
+
+interface AuthState {
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+const getInitialUser = () => {
+  if (typeof window !== "undefined") {
+    const storedUser = localStorage.getItem("user-musc");
+    return storedUser ? JSON.parse(storedUser) : null;
+  }
+  return null;
+};
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: getInitialUser(),
+  setUser: (user) => {
+    set({ user });
+    if (typeof window !== "undefined") {
+      if (user) {
+        localStorage.setItem("user-musc", JSON.stringify(user));
+      } else {
+        localStorage.removeItem("user-musc");
+      }
+    }
+  },
+}));
+// useAuthSubscription hook
+export function useAuthSubscription() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const unsubscribe = useAuthStore.subscribe((user) => {
+        if (user) {
+          localStorage.setItem("user-musc", JSON.stringify(user));
+        } else {
+          localStorage.removeItem("user-musc");
+        }
+      });
+
+      return () => unsubscribe();
+    }
+  }, []);
+}
