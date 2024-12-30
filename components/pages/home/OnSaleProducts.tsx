67a7c5f767ffd74292eaafe63cd3df68dc596c0e
Ahmed <ahmedabozaid.dev@gmai.com>
2024-12-30 20:50:08 +0200

    adds loading for products

--- components/pages/home/OnSaleProducts.tsx
+++ components/pages/home/OnSaleProducts.tsx
@@ -13,8 +13,13 @@ import {
 import routes from "@/lib/routes";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
+
 export default function OnSaleProducts() {
-  const { data: products } = useQuery(["bestSelling"], getBestSelling);
+  const { data: products, isLoading } = useQuery(
+    ["bestSelling"],
+    getBestSelling
+  );
+
   return (
     <>
       <div className="lg:py-[60px] py-[32px]">
@@ -34,28 +39,49 @@ export default function OnSaleProducts() {
             </Button>
           </div>
           <div className="mt-[56px]">
-            <Carousel>
-              <CarouselContent>
-                {products?.map((product) => (
-                  <CarouselItem
-                    key={product.id}
-                    className="lg:basis-[30%] basis-[85%]"
-                  >
-                    <ProductCard
-                      link={routes?.Product?.POST(product.id)}
-                      id={product.id}
-                      offer={product.discountPercentage}
-                      image={product.imageCover}
-                      category={"test"}
-                      name={product.title}
-                      price={product.priceAfterDiscount}
-                    />
-                  </CarouselItem>
-                ))}
-              </CarouselContent>
-              <CarouselPrevious className="p-1 justify-center" />
-              <CarouselNext className="p-1 justify-center" />
-            </Carousel>
+            {/* Skeleton Loader or Products */}
+            {isLoading ? (
+              <Carousel>
+                <CarouselContent>
+                  {[...Array(5)].map((_, index) => (
+                    <CarouselItem
+                      key={index}
+                      className="lg:basis-[30%] basis-[85%] relative"
+                    >
+                      {/* Skeleton for Product Card */}
+                      <div className="w-full h-[440px] bg-gray-300 animate-pulse rounded-lg mb-4"></div>
+                      <div className="w-[70%] h-[20px] bg-gray-300 animate-pulse rounded mb-2"></div>
+                      <div className="w-[50%] h-[20px] bg-gray-300 animate-pulse rounded"></div>
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="p-1 justify-center" />
+                <CarouselNext className="p-1 justify-center" />
+              </Carousel>
+            ) : (
+              <Carousel>
+                <CarouselContent>
+                  {products?.map((product) => (
+                    <CarouselItem
+                      key={product.id}
+                      className="lg:basis-[30%] basis-[85%]"
+                    >
+                      <ProductCard
+                        link={routes?.Product?.POST(product.id)}
+                        id={product.id}
+                        offer={product.discountPercentage}
+                        image={product.imageCover}
+                        category={"test"}
+                        name={product.title}
+                        price={product.priceAfterDiscount}
+                      />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+                <CarouselPrevious className="p-1 justify-center" />
+                <CarouselNext className="p-1 justify-center" />
+              </Carousel>
+            )}
           </div>
         </div>
       </div>
