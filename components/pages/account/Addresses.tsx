562eca617276438b96a528d601e003d654d75ab8
Ahmed <ahmedabozaid.dev@gmail.com>
2025-01-08 20:26:40 +0200

    integrates add address

--- components/pages/account/Addresses.tsx
+++ components/pages/account/Addresses.tsx
@@ -1,7 +1,8 @@
 import { AddressTypes } from "@/apiRequests/address/getAddresses";
+import AddAddressForm from "@/components/general/AddAddressForm";
 import AddNewAddress from "@/components/Icons/account/AddNewAddress";
 import AddressCard from "@/components/pages/account/AddressCard";
-import React from "react";
+import React, { useState } from "react";
 
 interface AddressesProps {
   addresses: AddressTypes[];
@@ -9,6 +10,7 @@ interface AddressesProps {
 }
 
 const Addresses = ({ addresses, refetch }: AddressesProps) => {
+  const [showAddForm, setShowAddForm] = useState(false);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7">
       {addresses.map((address) => (
@@ -23,10 +25,18 @@ const Addresses = ({ addresses, refetch }: AddressesProps) => {
         />
       ))}
 
-      <button className="text-2xl flex items-center  text-[#1E1E1E] font-medium px-[32px] py-[80px] bg-white rounded-[40px] max-w-[350px] border-2 border-[#808E98]">
+      <button
+        onClick={() => setShowAddForm(true)}
+        className="text-2xl flex items-center  text-[#1E1E1E] font-medium px-[32px] py-[80px] bg-white rounded-[40px] max-w-[350px] border-2 border-[#808E98]"
+      >
         <AddNewAddress />
         اضف عنوان جديد
       </button>
+      <AddAddressForm
+        showForm={showAddForm}
+        setShowForm={setShowAddForm}
+        refetch={refetch}
+      />
     </div>
   );
 };
