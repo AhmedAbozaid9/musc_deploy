0066baa0f40c1905bd8b5e6018fbee9f741be8f1
Ahmed <ahmedabozaid.dev@gmail.com>
2025-01-11 14:16:43 +0200

    fixes user flow when there's nothing

--- components/pages/account/Addresses.tsx
+++ components/pages/account/Addresses.tsx
@@ -5,7 +5,7 @@ import AddressCard from "@/components/pages/account/AddressCard";
 import React, { useState } from "react";
 
 interface AddressesProps {
-  addresses: AddressTypes[];
+  addresses?: AddressTypes[];
   refetch: () => void;
 }
 
@@ -13,17 +13,19 @@ const Addresses = ({ addresses, refetch }: AddressesProps) => {
   const [showAddForm, setShowAddForm] = useState(false);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7">
-      {addresses.map((address) => (
-        <AddressCard
-          key={address._id}
-          id={address._id}
-          name={address.fullName}
-          fullName={address.fullName}
-          address={address.detailedAddress}
-          phone={address.phoneNumber}
-          refetch={refetch}
-        />
-      ))}
+      {addresses &&
+        addresses.length > 0 &&
+        addresses.map((address) => (
+          <AddressCard
+            key={address._id}
+            id={address._id}
+            name={address.fullName}
+            fullName={address.fullName}
+            address={address.detailedAddress}
+            phone={address.phoneNumber}
+            refetch={refetch}
+          />
+        ))}
 
       <button
         onClick={() => setShowAddForm(true)}
