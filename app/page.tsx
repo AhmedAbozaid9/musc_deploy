1441aee1a78fba35d74a52159ee3b536efa16cd3
Ahmed <ahmedabozaid.dev@gmai.com>
2025-03-03 14:47:52 +0200

    adds social login

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,28 @@
+"use client";
+
 import BannerHero from "@/components/pages/home/BannerHero";
 import FeaturedProducts from "@/components/pages/home/FeaturedProducts";
 import OfferBanner from "@/components/pages/home/OfferBanner";
 import OnSaleProducts from "@/components/pages/home/OnSaleProducts";
 import ShopByCategory from "@/components/pages/home/ShopByCategory";
 import WhyChooseUs from "@/components/pages/home/WhyChooseUs";
-import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "الرئيسية | متجري",
-  description: "كل احتياجاتك من اللالكترونيات بأقوى الخصومات",
-};
+import { useAuthStore } from "@/store/useAuthStore";
+import Cookies from "js-cookie";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+  const setUser = useAuthStore((state) => state.setUser);
+
+  useEffect(() => {
+    if (token) {
+      Cookies.set("musc-token", token);
+      window.location.replace(window.location.pathname);
+    }
+  }, [token]);
+
   return (
     <>
       <BannerHero />
